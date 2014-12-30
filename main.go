71a6f2e7f6a218aff989ed22c47578637938338d
Ric Allinson <capecodehq@yahoo.co.uk>
2014-12-30 10:28:05 -0800

    Change the shade algo to generate 0-16

--- main.go
+++ main.go
@@ -11,14 +11,17 @@ import (
 	"strconv"
 )
 
+var SHADE_MAX = 16
+var SHADE_MIN = 1
+
 func getShade(c color.Color, pixelsize int) int {
 	r, g, b, _ := c.RGBA()
-	shade := int((r>>12 + g>>12 + b>>12) / 3)
-	if shade > 15 { // Not too light.
-		return 15
+	shade := int((r + g + b) / 3) / 4000 // Max is 65535
+	if shade > SHADE_MAX {
+		return SHADE_MAX
 	}
-	if shade < 0 { // Not too dark.
-		return 0
+	if shade < SHADE_MIN {
+		return SHADE_MIN
 	}
 	return shade
 }
@@ -26,11 +29,11 @@ func getShade(c color.Color, pixelsize int) int {
 func pixelPulse(x int, y int, shade int, pixelsize int) (cmds string) {
 	pixelhalf := pixelsize/2
 	cmds += "M " + strconv.Itoa(x) + " " + strconv.Itoa(y+pixelhalf) + "\n"
-	if shade >= 15 {
+	if shade >= SHADE_MAX {
 		cmds += "L " + strconv.Itoa(x+pixelsize) + " " + strconv.Itoa(y+pixelhalf) + "\n"
 		return cmds
 	}
-	offset := shade + 5
+	offset := shade + 4 % pixelsize
 	down := true
 	for xoff := x; xoff < x+pixelsize; xoff = xoff + offset {
 		if down {
@@ -45,12 +48,35 @@ func pixelPulse(x int, y int, shade int, pixelsize int) (cmds string) {
 	return cmds
 }
 
+func pixelQuake(x int, y int, shade int, pixelsize int) (cmds string) {
+	pixelhalf := pixelsize/2
+	cmds += "M " + strconv.Itoa(x) + " " + strconv.Itoa(y+pixelhalf) + "\n"
+	if shade >= SHADE_MAX {
+		cmds += "L " + strconv.Itoa(x+pixelsize) + " " + strconv.Itoa(y+pixelhalf) + "\n"
+		return cmds
+	}
+	offset := shade + 4 % pixelsize
+	down := true
+	for xoff := x; xoff < x+pixelsize; xoff = xoff + offset {
+		if down {
+			cmds += "L " + strconv.Itoa(xoff) + " " + strconv.Itoa(y+pixelsize) + "\n"
+			down = false
+		} else {
+			cmds += "L " + strconv.Itoa(xoff) + " " + strconv.Itoa(y) + "\n"
+			down = true
+		}
+	}
+	cmds += "L " + strconv.Itoa(x+pixelsize) + " " + strconv.Itoa(y+pixelhalf) + "\n"
+	return cmds
+}
+
 func pixelSaw(x int, y int, shade int, pixelsize int) (cmds string) {
-	if shade >= 15 {
+	if shade >= SHADE_MAX {
 		return "M " + strconv.Itoa(x+pixelsize) + " " + strconv.Itoa(y) + "\n"
 	}
+	offset := shade + 4 % pixelsize
 	down := true
-	for xoff := x; xoff < x+pixelsize; xoff = xoff + shade {
+	for xoff := x; xoff < x+pixelsize; xoff = xoff + offset {
 		if down {
 			cmds += "L " + strconv.Itoa(xoff) + " " + strconv.Itoa(y+pixelsize) + "\n"
 			down = false
@@ -73,12 +99,12 @@ func convert(file *os.File, xoffset int, yoffset int, pixelsize int) (string, er
 		plots += "M " + strconv.Itoa(xoffset) + " " + strconv.Itoa((y*pixelsize)+yoffset) + "\n"
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
 			shade := getShade(img.At(x, y), pixelsize)
-			plots += pixelPulse((x*pixelsize)+xoffset, (y*pixelsize)+yoffset, shade, pixelsize)
-			fmt.Printf("%02d", shade)
+			plots += pixelQuake((x*pixelsize)+xoffset, (y*pixelsize)+yoffset, shade, pixelsize)
+			fmt.Printf("%02d,", shade)
 		}
 		fmt.Print("\n")
 	}
-	// plots += "h"
+	plots += "h\n"
 	return plots, nil
 }
 
